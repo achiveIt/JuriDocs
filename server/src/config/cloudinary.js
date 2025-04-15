e46f3885f7db277767dcf1e70f29a17939281219
achiveIt <happyface979797@gmail.com>
2025-04-15 17:53:14 +0530

    feat: add cloudinary.js in config

---
+++ server/src/config/cloudinary.js
@@ -0,0 +1,38 @@
+import { v2 as cloudinary } from "cloudinary";
+import { CloudinaryStorage } from 'multer-storage-cloudinary';
+import multer from 'multer';
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const storage = new CloudinaryStorage({
+    cloudinary: cloudinary,
+    params: {
+      folder: 'pdf-management-system/pdfs',
+      resource_type: 'raw',
+      public_id: (req, file) => {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        const filename = file.originalname.split('.')[0];
+        return `${filename}-${uniqueSuffix}`;
+      }
+    }
+  });
+
+  const fileFilter = (req, file, cb) => {
+    if (file.mimetype === 'application/pdf') {
+      cb(null, true);
+    } else {
+      cb(new Error('Only PDF files are allowed'), false);
+    }
+  };
+
+  export const upload = multer({
+    storage: storage,
+    fileFilter: fileFilter,
+    limits: { fileSize: 10 * 1024 * 1024 }
+  });
+  
+  export { cloudinary };
