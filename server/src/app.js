3476c0d304f8a92c26d91c7801e0dabb3e1c401f
achiveIt <happyface979797@gmail.com>
2025-04-19 10:01:06 +0530

    feat: add commenting on shared files

--- server/src/app.js
+++ server/src/app.js
@@ -4,7 +4,8 @@ import authRoutes from './routes/authRoutes.js';
 import pdfRoutes from './routes/pdfRoutes.js';
 import commentRoutes from './routes/commentRoutes.js'
 import bodyParser from 'body-parser';
-import cors from 'cors'
+import cors from 'cors';
+import sharedRoutes from './routes/sharedRoutes.js';
 
 const app = express();
 
@@ -13,10 +14,11 @@ app.use(express.json({limit:"16kb"}));
 app.use(cookieParser());
 
 app.use(bodyParser.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/pdf', pdfRoutes)
 app.use('/api/comment', commentRoutes)
+app.use('/api/shared', sharedRoutes)
 
 export {app}
